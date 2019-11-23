cc2bacc89497761e4afe80eb0b39828a9465643a
lwooden <lowell.wooden@gmail.com>
2019-11-23 11:51:24 -0500

    commented out because users wont access this manually (dymanic route)

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
       <h1>Expensify</h1>
       <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>  {/* NavLink allows for easier styling */}
       <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+      {/* <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> */}
       <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 )
