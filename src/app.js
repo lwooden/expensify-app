85c8de37a7afdcdad43a3c685434e602669c7719
lwooden <lowell.wooden@gmail.com>
2019-11-23 11:01:15 -0500

    introduced a header; linked pages using "Link" component; added comments

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
@@ -23,6 +23,7 @@ const EditExpensePage = () => (
   </div>
 )
 
+
 const HelpPage = () => (
   <div>
     This is from my help component
@@ -31,19 +32,34 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
   <div>
-    404!
+    404 - <Link to="/">Go Home</Link> {/* enables client-side routing -> new call to ReactDOM.render() instead of full refresh */}
   </div>
 )
 
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to="/">Dashboard</Link>
+    <Link to="/create">Create Expense</Link>
+    <Link to="/edit">Edit Expense</Link>
+    <Link to="/help">Help</Link>
+  </header>
+)
+
+
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+  {/* Single root element inside of BrowserRouter */}
+  <div>
+    <Header /> {/* Displayed on every page */}
+    <Switch> {/* Processes routes exclusively for betting matching */}
+      <Route path="/" component={ExpenseDashboardPage} exact={true} /> {/* must be typed exactly in order to render */}
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
       <Route component={NotFoundPage} />
     </Switch>
+  </div>
   </BrowserRouter>
 )
 
