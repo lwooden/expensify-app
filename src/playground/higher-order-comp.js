8ba3f312d1e23a756a7f748d28de9da7e86f22c5
lwooden <lowell.wooden@gmail.com>
2019-12-22 09:44:00 -0500

    higher order component example

---
+++ src/playground/higher-order-comp.js
@@ -0,0 +1,40 @@
+// Higher Order Component (HOC)
+// A component that renders another component
+// Render Hijacking, Prop Manipulation, Abstract State
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Info = props => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            { props.isAdmin && <p>This is private info please dont share!</p> }
+            <WrappedComponent {...props} />
+        </div>
+    )
+
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? ( <WrappedComponent {...props} /> ) : ( <p>Please log in to see info!</p> )}
+        </div>
+
+    )
+
+}
+
+const AdminInfo = withAdminWarning(Info)
+const AuthInfo = requireAuthentication(Info)
+
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="Here are the details" />, document.getElementById('app'))
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="Here is the info"  />, document.getElementById('app'))
