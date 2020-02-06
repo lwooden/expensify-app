92aecd348727fa35091cf593c7f01750e420f830
lwooden <lowell.wooden@gmail.com>
2020-02-06 11:14:53 -0500

    connect component to redux store and pass props to it

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import ExpenseForm from '../components/ExpenseForm'
+import { addExpense } from '../actions/expenses'
 
+// 1. Import connect so component can have access to the store
+// 2. Create a "connected" version of the component
+// 3. Wire up props so I can access dispatch method
+// 4. Import addExpense from action folder
+// 5. Take new expense object and dispatch it to store using addExpense function
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
     <div>
     <h1>Add Expense</h1>
-      <ExpenseForm />
+      <ExpenseForm 
+        onSubmit={(expense) => {
+          console.log(expense)
+          props.dispatch(addExpense(expense))
+          props.history.push('/') // automatically redirects me to the page I want the user to go to
+        }}
+      />
     </div>
   )
 
-export default AddExpensePage
+export default connect()(AddExpensePage)
