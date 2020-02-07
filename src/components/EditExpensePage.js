de54d789d42665ffa9cb03feb470f07eb1c0c95a
lwooden <lowell.wooden@gmail.com>
2020-02-07 17:12:42 -0500

    name change + add remove button + added props prefix where needed

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense, removeExpense } from "../actions/expenses";
+
+const EditExpensePage = props => {
+  console.log(props); // inspect default props that are passed down by react-router "Route" component
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense} // passing the expense object "down" into the ExpenseForm Component as a prop
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push("/");
+          console.log("updated", expense);
+        }}
+      />
+      <button
+        onClick={() => {
+          console.log(props.expense.id);
+          props.dispatch(removeExpense({ id: props.expense.id }));
+          props.history.push("/");
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(expense => {
+      return expense.id === props.match.params.id;
+    })
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
